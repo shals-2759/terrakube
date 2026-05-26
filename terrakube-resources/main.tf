762f1d8a52780b2e8efad44b13db006a07ecb085
Shalini R <shalini.r@jmangroup.com>
2026-05-26 17:45:50 +0530

    new resource

--- terrakube-resources/main.tf
+++ terrakube-resources/main.tf
@@ -1,12 +1,10 @@
-resource "azurerm_resource_group" "example"{
-  name = "rg-prac"
-  location = "Central India"
-}
 
 resource "azurerm_virtual_network" "new"{
     name="vn-prac"
-    resource_group_name = "rg-prac"
-    location = "Central India"
+    resource_group_name = "rg-tk-idp-dev-uksouth-01"
+    location = "UK South"
     address_space = ["10.0.1.0/16"]
 }
+
+
 
