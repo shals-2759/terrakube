c2d7ce879f92a93470882a1b5697ce29b39533b2
Shalini R <shalini.r@jmangroup.com>
2026-05-26 20:05:24 +0530

    new network address

--- terrakube-resources/main.tf
+++ terrakube-resources/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "new"{
     name="vn-prac"
     resource_group_name = "rg-tk-idp-dev-uksouth-01"
     location = "UK South"
-    address_space = ["10.0.1.0/16"]
+    address_space = ["10.0.0.0/16"]
 }
 
 
